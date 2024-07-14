d58e911045c590e0cb673b1dc8b9b8ffc1e7f0db
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-07-13 22:12:43 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
 //document.getElementsByTagName('head')[0].appendChild(script);
+
+let translate = document.getElementById("translate").addEventListener("mouseout",
+    function ()
+    {
+        this.style.border = "1px solid #000";
+    }
+);
